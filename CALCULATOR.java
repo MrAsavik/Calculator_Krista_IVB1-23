781615094ba03a6ebdec8ce403c7664473531c44
iarik <nuimovs@gmail.com>
2025-02-25 09:43:44 +0300

    feat(input): формил case, без исключений

--- CALCULATOR.java
+++ CALCULATOR.java
@@ -15,10 +15,23 @@ public class CALCULATOR
         switch (actionNumber)
         {
         
-            case 1:
+            case 1: 
+                System.out.print(variableFirst + variableSecond);
+                break;
             case 2:
+                System.out.print(variableFirst - variableSecond);
+                break;
             case 3:
+                System.out.print(variableFirst * variableSecond);
+                break;
             case 4:
+                if (variableSecond != 0)
+                {
+                    System.out.print(variableFirst / variableSecond);
+                }
+                else System.out.println("Нельзя делить на ноль");
+                
+                break;
             default: System.out.println("Ошибка ввода данных");
         }
 
