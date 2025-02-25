09f20d4f68da7627d0d9c058d101375e0d06e3ac
iarik <nuimovs@gmail.com>
2025-02-25 09:17:36 +0300

    feat(input): Добавил ввод первого числа

--- CALCULATOR.java
+++ CALCULATOR.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class CALCULATOR
 {
     public static void main(String[] args)
     {
-        System.out.print("Hello");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello");
+        System.out.print("Введите чило: ");
+        float variableOne = scanner.nextFloat();
+        
+
     }
 }
